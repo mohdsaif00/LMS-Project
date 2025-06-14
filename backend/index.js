02e9021a1d36c9379012eb5eadce4500631e4774
mohd saif <msaifansari13@gmail.com>
2025-06-14 15:08:00 +0530

    adding  some code in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,17 @@
+import express from 'express'
 
+import dotenv from 'dotenv'
+dotenv.config({ path: './.env' })
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+      res.send("This is home page")
+})
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+      console.log(`server running on http://localhost:${PORT}`);
+})
