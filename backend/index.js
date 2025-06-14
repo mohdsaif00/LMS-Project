db92aef50e63b8a66ebe76cfd404a79a727ea798
mohd saif <msaifansari13@gmail.com>
2025-06-14 16:05:51 +0530

    saif -> register backend code done

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,18 @@
 import express from 'express'
 
+import userRouter from "./routes/user.route.js"
+
 import dotenv from 'dotenv'
 dotenv.config({ path: './.env' })
 
 const app = express()
-
+app.use(express.json())
 
 app.get('/', (req, res) => {
       res.send("This is home page")
 })
+
+app.use("/user", userRouter)
 
 const PORT = process.env.PORT
 
