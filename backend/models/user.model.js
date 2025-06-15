4388c895aad08bad59b8ed17181f06e4926f282f
mohd saif <msaifansari13@gmail.com>
2025-06-16 02:07:42 +0530

    adding reset password and forgot password code

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema({
   name: {
@@ -17,7 +18,24 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  resetOtp:{
+    type: Number
+  },
+  resetOtpExp:{
+    type: Number``
+  }
 });
+
+userSchema.pre('save', async function(next) {
+  if(this.isModified('password')){
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+})
+
+userSchema.methods.comparePass = async function (inputPass) {
+  return await bcrypt.compare(inputPass, this.password) 
+}
 
 const UserModel = mongoose.model('User', userSchema);
 export default UserModel;
