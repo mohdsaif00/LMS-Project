5953d59c9cc359c7c120f0b01bfa07573f8b2f72
MDTAUSIFALAM9123 <mdtausifalam9123@gmail.com>
2025-06-15 17:40:18 +0530

    Tausif-> Frontend  Register Login Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css'
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
 function App() {
 
   return (
-    <>
-      <div>Hello</div>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
