7b832590fe9bdfd870ad80d6939c6e7516859a04
mohd saif <msaifansari13@gmail.com>
2025-06-15 15:04:46 +0530

    del some code

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -97,24 +97,3 @@ export async function login(req, res) {
   });
 }
 
-export async function logout(req, res) {
-  const userId = req.userId;
-
-  if (!userId) {
-    return res.json({
-      msg: 'please provide id',
-    });
-  }
-
-  const cookieOption = {
-    httpOnly: true,
-    source: process.env.NODE_ENV,
-    sameSite: 'strict',
-    maxAge: 7 * 24 * 60 * 60 * 100,
-  };
-  res.clearCookie('accessToken', cookieOption);
-
-  return res.json({
-    message: 'Logged Out',
-  });
-}
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,9 @@
 import { Router } from 'express';
-import { login, register, logout } from '../controllers/user.controller.js';
+import { login, register, } from '../controllers/user.controller.js';
 
 const userRouter = Router();
 
 userRouter.post('/register', register);
 userRouter.post('/login', login);
-userRouter.post('/logout', logout);
 
 export default userRouter;
