d7abce6cf1121f2d2874b1b7f36de26f83185fbb
MDTAUSIFALAM9123 <mdtausifalam9123@gmail.com>
2025-06-16 10:22:40 +0530

    First commit

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,13 @@ import { config } from 'dotenv';
 config();
 import connectionToDB from './config/dbConnection.js';
 import userRouter from './routes/user.route.js';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+}));
 const PORT = process.env.PORT;
 
 app.use('/api', userRouter);
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -4,16 +4,19 @@ const userSchema = mongoose.Schema({
   name: {
     type: String,
     required: true,
+    trim: true,
   },
   email: {
     type: String,
     required: true,
     unique: true,
+    trim: true,
   },
 
   password: {
     type: String,
     required: true,
+
   },
   phone: {
     type: Number,
