51ac94b7f6a12472b36b81debe518cfb9c206c6f
mohd saif <msaifansari13@gmail.com>
2025-06-18 04:31:51 +0530

    saif-> my work done in home page home section,company history,course categories

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Register from './pages/Register'
 import Login from './pages/Login'
+
+
 function App() {
 
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/home" element={<Home />} />
+    <Routes> 
+      <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
-    </Routes>
+    </Routes> 
     </BrowserRouter>
   )
 }
