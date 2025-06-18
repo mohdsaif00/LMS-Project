c0ed2d5c270d6e288a5b2bd4aeaf55d5a4df303c
MDTAUSIFALAM9123 <mdtausifalam9123@gmail.com>
2025-06-18 21:53:58 +0530

    Home page Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,11 +8,7 @@ function App() {
 
   return (
     <BrowserRouter>
-<<<<<<< HEAD
     <Routes>
-=======
-    <Routes> 
->>>>>>> c3b9abe41dd0c163d797a4989df22b3d3d9a5ffd
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
--- frontend/src/components/course-categories.jsx
+++ frontend/src/components/course-categories.jsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-import React from 'react';
->>>>>>> c3b9abe41dd0c163d797a4989df22b3d3d9a5ffd
 import { Brain, Code, Monitor, Cpu, Palette, Cloud, Target, Shield } from 'lucide-react';
 
 const CourseCategoriesSection = () => {
@@ -65,11 +61,7 @@ const CourseCategoriesSection = () => {
   ];
 
   return (
-<<<<<<< HEAD
     <div className="min-h-xl bg-gray-50 py-16 px-4">
-=======
-    <div className="min-h-screen bg-gray-50 py-16 px-4">
->>>>>>> c3b9abe41dd0c163d797a4989df22b3d3d9a5ffd
       <div className="max-w-7xl mx-auto">
         {/* Header Section */}
         <div className="text-center mb-12">
@@ -130,6 +122,6 @@ const CourseCategoriesSection = () => {
       </div>
     </div>
   );
-};
+}
 
 export default CourseCategoriesSection;
