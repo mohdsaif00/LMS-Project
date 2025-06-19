96a4dc9115c1e5152c4830ec0075b80a5f6e780d
mohd saif <msaifansari13@gmail.com>
2025-06-20 04:28:17 +0530

    fix: update home route to use root path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
     </Routes>
