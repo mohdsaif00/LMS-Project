f745381058b01e42dc41b34faed13a53fb064031
MDTAUSIFALAM9123 <mdtausifalam9123@gmail.com>
2025-06-21 07:11:33 +0530

    Tausif->Update

--- backend/utils/nodemailer.js
+++ backend/utils/nodemailer.js
@@ -10,8 +10,4 @@ export const mailer = nodemailer.createTransport({
     user: process.env.SMTP_USERNAME, // Your email address
     pass: process.env.SMTP_PASSWORD, // Use App Password if using Gmail
   },
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 528dc6ddd26f0f9eb55dba76bb6e4d60c0642129
